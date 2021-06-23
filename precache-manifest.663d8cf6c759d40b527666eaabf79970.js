2e3207230a4874447c25a45321836e2413799710
Rida Khan <rida.khan5@hotmail.com>
2021-06-23 16:40:17 +0400

    Updates

---
+++ precache-manifest.663d8cf6c759d40b527666eaabf79970.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f07c3ca8d64db04d452df537d9b02819",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "2bbae6fa4172ce3a2c0c",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4e1af961ec1c694bfcf8",
+    "url": "/home/static/css/main.1321dd01.chunk.css"
+  },
+  {
+    "revision": "2bbae6fa4172ce3a2c0c",
+    "url": "/home/static/js/2.e3a270fa.chunk.js"
+  },
+  {
+    "revision": "9371b595e371c0d4c4f99812c6aece96",
+    "url": "/home/static/js/2.e3a270fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e1af961ec1c694bfcf8",
+    "url": "/home/static/js/main.9f2b834f.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "68be1b44bb00c146f5ca21dc7945af0c",
+    "url": "/home/static/media/Profile.68be1b44.jpg"
+  },
+  {
+    "revision": "55f4ef0baec89dda4bc3eed4bee7bc4f",
+    "url": "/home/static/media/boeing.55f4ef0b.jpg"
+  },
+  {
+    "revision": "433b371aabb4a133cfc9682923d5d0b3",
+    "url": "/home/static/media/culture.433b371a.jpg"
+  },
+  {
+    "revision": "5d4d169ec7e28d53d5c95bae3f756960",
+    "url": "/home/static/media/dell-logo.5d4d169e.jpg"
+  },
+  {
+    "revision": "53c4c973817430ff2ed8403e67f7bbee",
+    "url": "/home/static/media/motion.53c4c973.png"
+  },
+  {
+    "revision": "33f31b4ce606a697796e2a9585fafeba",
+    "url": "/home/static/media/resume.33f31b4c.pdf"
+  },
+  {
+    "revision": "167f94d9e466e8adbfe48391437e3842",
+    "url": "/home/static/media/sentiment.167f94d9.png"
+  },
+  {
+    "revision": "d6698c9280cebf61b105369c173a8732",
+    "url": "/home/static/media/service.d6698c92.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52027cbb359ce95b0d0a7cf596ed3528.js"
+  "/home/precache-manifest.663d8cf6c759d40b527666eaabf79970.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
